64a1c29cce28cfeaa3829623a251da1bc889b901
Paolo Santillo <paolosantillo@yahoo.it>
2026-06-09 07:27:08 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,1 +1,25 @@
+importScripts("https://www.gstatic.com/firebasejs/12.13.0/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/12.13.0/firebase-messaging-compat.js");
 
+firebase.initializeApp({
+  apiKey: "AIzaSyBCKQp_DA2Bjbs6g27Wwl8eo_kyzzI2A40",
+  authDomain: "calendario-rep.firebaseapp.com",
+  projectId: "calendario-rep",
+  storageBucket: "calendario-rep.firebasestorage.app",
+  messagingSenderId: "1067128179274",
+  appId: "1:1067128179274:web:e1c7174c25bdabee2ff4b3"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+
+  self.registration.showNotification(
+    payload.notification.title,
+    {
+      body: payload.notification.body,
+      icon: "/icon.png"
+    }
+  );
+
+});
