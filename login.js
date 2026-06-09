d2cde00a1b6968bb68adfed9b66836d6343035d8
Paolo Santillo <paolosantillo@yahoo.it>
2026-06-09 23:59:14 +0200

    Update login.js

--- login.js
+++ login.js
@@ -1,57 +1,40 @@
 
-import { initializeApp }
+import { initializeApp } 
 from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 
-import {
-  getAuth,
-  signInWithEmailAndPassword
-}
+import { 
+  getAuth, 
+  signInWithEmailAndPassword 
+} 
 from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
 
 const firebaseConfig = {
-
   apiKey: "AIzaSyBCKQp_DA2Bjbs6g27Wwl8eo_kyzzI2A40",
-
   authDomain: "calendario-rep.firebaseapp.com",
-
   projectId: "calendario-rep",
-
   storageBucket: "calendario-rep.firebasestorage.app",
-
   messagingSenderId: "1067128179274",
-
   appId: "1:1067128179274:web:e1c7174c25bdabee2ff4b3"
-
 };
 
-const app =
-  initializeApp(firebaseConfig);
+// INIT FIREBASE
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
 
-const auth =
-  getAuth(app);
+// FUNZIONE LOGIN
+window.login = async function () {
 
-window.login = async function(){
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-  const email =
-    document.getElementById("email").value;
-
-  const password =
-    document.getElementById("password").value;
-
-  try{
-
+  try {
     await signInWithEmailAndPassword(auth, email, password);
 
-window.location.replace("index.html");
+    // 👉 vai alla home DOPO login riuscito
+    window.location.href = "index.html";
 
-  }catch(error){
-
-    alert(
-      "Email o password errati"
-    );
-
+  } catch (error) {
     console.error(error);
-
+    alert("Email o password errati");
   }
-
 };
