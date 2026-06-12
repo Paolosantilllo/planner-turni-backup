ab6576f7150daf8a16f2c3612c5b9f9162b9cddb
Paolo Santillo <paolosantillo@yahoo.it>
2026-06-12 21:19:04 +0200

    Update app.js

--- app.js
+++ app.js
@@ -704,8 +704,19 @@ for (let d = 1; d <= daysInMonth; d++) {
 }
 
 const head = [
-  ["Nominativi", ...giorniNumeri],
-  ["" , ...giorniSettimana]
+  [
+    "Nominativi",
+    ...Array.from({ length: daysInMonth }, (_, i) => {
+      const day = i + 1;
+
+      const date = new Date(year, month, day);
+      const jsDay = date.getDay();
+
+      const map = ["D", "L", "Ma", "Me", "G", "V", "S"];
+
+      return `${day}\n${map[jsDay]}`;
+    })
+  ]
 ];
 
   const dipendenti = Object.keys(EMPLOYEES);
