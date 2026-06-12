6da50b8e3dd3db135d7cbc6f82f96cbe0a7a0e4d
Paolo Santillo <paolosantillo@yahoo.it>
2026-06-12 10:01:35 +0200

    Update employees.js

--- employees.js
+++ employees.js
@@ -1,1 +1,23 @@
 
+/* ======================
+   DIPENDENTI CENTRALIZZATI
+====================== */
+
+export const EMPLOYEES = {
+  A: {
+    name: "SANTILLO",
+    color: "dipendente-santillo"
+  },
+  B: {
+    name: "MANUNTA",
+    color: "dipendente-b"
+  },
+  C: {
+    name: "DIPENDENTE C",
+    color: "dipendente-c"
+  },
+  D: {
+    name: "DIPENDENTE D",
+    color: "dipendente-d"
+  }
+};
