8daaf3544ba9c1c6e0bfeadbb3ab848643761b11
Paolo Santillo <paolosantillo@yahoo.it>
2026-06-13 23:25:53 +0200

    Update employees.js

--- employees.js
+++ employees.js
@@ -21,3 +21,17 @@ export const EMPLOYEES = {
     color: "dipendente-d"
   }
 };
+
+/* ======================
+   COLORI TURNI (NUOVO)
+====================== */
+
+export const SHIFT_COLORS = {
+  REP: "#F8BBD0",       // rosa
+  FREP: "#F8BBD0",      // rosa
+  CFI: "#66BB6A",       // verde
+  "CFI/REP": "#66BB6A", // verde
+  LIC: "#FFEB3B",       // giallo
+  REC: "#FFEB3B",       // giallo
+  MAL: "#E0E0E0"        // grigio
+};
