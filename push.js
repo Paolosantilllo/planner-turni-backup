5becaed78a3a303b85ad8d7dd28b7765bde7e82d
Paolo Santillo <paolosantillo@yahoo.it>
2026-06-24 19:05:30 +0200

    Update push.js

--- push.js
+++ push.js
@@ -94,3 +94,24 @@ export function listenForegroundNotifications() {
   });
 
 }
+
+
+// ======================
+// 🔔 BOTTONE ATTIVA PUSH
+// ======================
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const btn = document.getElementById("enablePushBtn");
+
+  if(btn){
+
+    btn.addEventListener("click", () => {
+
+      initPush(window.CURRENT_USER);
+
+    });
+
+  }
+
+});
